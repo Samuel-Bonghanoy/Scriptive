48283582e94319f3a4067752e4f6ae214112076d
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-31 02:15:22 +0800

    feat: created a navbar

--- components/ui/stats.jsx
+++ components/ui/stats.jsx
@@ -4,42 +4,14 @@ function Stats() {
   return (
     <div className="stats shadow h-[80%]">
       <div className="stat">
-        <div className="stat-figure text-primary">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-8 h-8 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-            ></path>
-          </svg>
-        </div>
+        <div className="stat-figure text-primary"></div>
         <div className="stat-title">Total Likes</div>
         <div className="stat-value text-primary">25.6K</div>
         <div className="stat-desc">21% more than last month</div>
       </div>
 
       <div className="stat">
-        <div className="stat-figure text-secondary">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-8 h-8 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 10V3L4 14h7v7l9-11h-7z"
-            ></path>
-          </svg>
-        </div>
+        <div className="stat-figure text-secondary"></div>
         <div className="stat-title">Page Views</div>
         <div className="stat-value text-secondary">2.6M</div>
         <div className="stat-desc">21% more than last month</div>
@@ -48,15 +20,7 @@ function Stats() {
       <div className="stat">
         <div className="stat-figure text-secondary">
           <div className="avatar online">
-            <div className="w-16 rounded-full">
-              <Image
-                src="/hero.svg"
-                alt="Next.js Logo"
-                width={670}
-                height={400}
-                priority
-              />
-            </div>
+            {/* <div className="w-16 rounded-full"></div> */}
           </div>
         </div>
         <div className="stat-value">86%</div>
