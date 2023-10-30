540c97225b7f0c2ed4ef92682cd6ab22b406538e
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-31 01:47:10 +0800

    feat: created a navbar

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -4,18 +4,16 @@ import { rubik } from "@/app/layout";
 export default function Hero() {
   return (
     <div className="flex flex-col gap-6 w-[60%]">
-      <h1
-        className={`${rubik.className} font-bold text-6xl mt-4 text-white tracking-wider`}
-      >
+      <h1 className={`font-bold text-6xl mt-4 text-white tracking-wider`}>
         SCRIPTIVE
       </h1>
-      <p className="text-black text-5xl font-bold max-w-[45ch] mt-6 leading-snug">
+      <p className="text-black text-[2.8rem] font-bold max-w-[45ch] mt-4 leading-snug">
         Optimize your communication, streamline your thoughts with automated
         letter crafting.
       </p>
-      <p className="text-white text-xl font-bold max-w-[45ch] leading-snug">
-        Optimize your communication, streamline your thoughts with automated
-        letter crafting.
+      <p className="text-white text-[1.35rem] font-bold max-w-[45ch] leading-snug">
+        A web application engineered to automate the creation of personalized
+        letters and documents.
       </p>
     </div>
   );
