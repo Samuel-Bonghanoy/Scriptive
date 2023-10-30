0572f33dd0e0a026419ac9b614907f6097633927
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-31 00:58:31 +0800

    feat: created a navbar

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -3,8 +3,10 @@ import { rubik } from "@/app/layout";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col">
-      <h1 className={`${rubik.className} font-black text-5xl`}>Scriptive</h1>
+    <div className="flex flex-col gap-6">
+      <h1 className={`${rubik.className} font-black text-5xl text-maindark`}>
+        Scriptive
+      </h1>
       <p>
         Scriptive Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Libero, ratione.
