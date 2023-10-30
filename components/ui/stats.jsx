43152060c4e1a27ea5f7f285fe774431ff34f503
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-31 02:20:13 +0800

    feat: created a navbar

--- components/ui/stats.jsx
+++ components/ui/stats.jsx
@@ -2,30 +2,32 @@ import Image from "next/image";
 
 function Stats() {
   return (
-    <div className="stats shadow h-[80%]">
+    <div className="stats shadow h-[80%] w-[70%] text-white">
       <div className="stat flex flex-col gap-3 justify-center">
-        <div className="stat-figure text-primary"></div>
-        <div className="stat-title">Total Likes</div>
-        <div className="stat-value text-primary">25.6K</div>
-        <div className="stat-desc">21% more than last month</div>
-      </div>
-
-      <div className="stat">
-        <div className="stat-figure text-secondary"></div>
-        <div className="stat-title">Page Views</div>
-        <div className="stat-value text-secondary">2.6M</div>
-        <div className="stat-desc">21% more than last month</div>
-      </div>
-
-      <div className="stat">
-        <div className="stat-figure text-secondary">
-          <div className="avatar online">
-            {/* <div className="w-16 rounded-full"></div> */}
-          </div>
+        <div className="flex flex-col gap-3">
+          <div className="stat-figure text-primary"></div>
+          <div className="stat-title">Total Likes</div>
+          <div className="stat-value text-main">25.6K</div>
+          <div className="stat-desc">21% more than last month</div>
         </div>
-        <div className="stat-value">86%</div>
-        <div className="stat-title">Tasks done</div>
-        <div className="stat-desc text-secondary">31 tasks remaining</div>
+      </div>
+
+      <div className="stat flex flex-col gap-3 justify-center">
+        <div className="flex flex-col gap-3">
+          <div className="stat-figure text-secondary"></div>
+          <div className="stat-title">Page Views</div>
+          <div className="stat-value  text-main">2.6M</div>
+          <div className="stat-desc">21% more than last month</div>
+        </div>
+      </div>
+
+      <div className="stat flex flex-col gap-3 justify-center">
+        <div className="flex flex-col gap-3">
+          <div className="stat-figure text-secondary"></div>
+          <div className="stat-title">Page Views</div>
+          <div className="stat-value text-main">2.6M</div>
+          <div className="stat-desc">31 tasks remaining</div>
+        </div>
       </div>
     </div>
   );
