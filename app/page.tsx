69196f12c9677e5ff57541b7f8653eb17bfa9d12
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-30 16:33:58 +0800

    feat: styled root page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image
-        className="border-black border-4 transition-all duration-[30]  hover:border-l-8 hover:border-b-8 rounded-md"
+        className="border-black border-2 transition-all duration-[30] hover:scale-[1.03] hover:bg-maindark hover:border-l-[6px] hover:border-b-[6px] rounded-md"
         src="/heroicon.svg"
         alt="Next.js Logo"
         width={300}
