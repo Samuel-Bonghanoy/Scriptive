b2ab976f401464362747e2120036ca1f618927a3
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-31 20:58:38 +0800

    feat: add edit view

--- app/templates/[type]/page.tsx
+++ app/templates/[type]/page.tsx
@@ -1,9 +1,9 @@
-import FormalLetter from "@/components/letters/formalLetter";
+import FormalLetter from "@/components/edit/formalLetter";
 import React from "react";
 
 export default function page() {
   return (
-    <div className="grid grid-cols-2 h-[120vh]">
+    <div className="grid grid-cols-[40vw_1fr] py-10 justify-items-center bg-main">
       <div className="bg-main">page</div>
       <FormalLetter />
     </div>
