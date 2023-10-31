bf5f832ef81f31a9e2eb230d000f1e1042fb457f
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-31 14:15:14 +0800

    feat: add a templates route

--- app/templates/layout.tsx
+++ app/templates/layout.tsx
@@ -2,8 +2,13 @@ import Navbar from "@/components/ui/navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    <div className="flex h-screen flex-col md:overflow-hidden">
       <Navbar />
+      <div className="grid grid-cols-3">
+        <div>a</div>
+        <div>a</div>
+        <div>a</div>
+      </div>
     </div>
   );
 }
