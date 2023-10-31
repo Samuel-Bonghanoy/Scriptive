ab8f78db71d52ff231b2a276eeba6b6755041c4f
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-31 16:31:07 +0800

    feat: add a templates route

--- components/letters/informalLetter.tsx
+++ components/letters/informalLetter.tsx
@@ -2,29 +2,16 @@ function InformalLetter() {
   return (
     <div className="flex flex-col px-5 py-5 text-black gap-2">
       <div className="flex flex-col">
-        <h1 className="text-xl font-bold">John Smith</h1>
-        <div className="flex justify-between">
-          <p className="text-xs font-semibold">Software Engineer</p>
+        <div className="flex flex-col justify-end">
+          <h1 className="text-xl font-bold self-end">John Smith</h1>
           <div className="flex flex-col">
             <p className="text-[0.65rem] self-end">+123-456-7890</p>
             <p className="text-[0.65rem] self-end">johnsmith@scriptive.com</p>
             <p className="text-[0.65rem] self-end">123 Anywhere St. Any City</p>
           </div>
         </div>
-        <hr className="border-1 border-black my-3" />
-        <p className="text-[0.6rem] self-end">14th August 2026</p>
-        <p className="text-[0.6rem] font-semibold">Recipient Name</p>
-        <p className="text-[0.6rem] font-semibold mb-2">
-          Hiring Manager, Scriptive HQ
-        </p>
 
-        <p className="text-[0.6rem] ">+123-456-7890</p>
-        <p className="text-[0.6rem] ">johnsmith@scriptive.com</p>
-        <p className="text-[0.6rem] ">123 Anywhere St. Any City</p>
-
-        <p className="text-[0.85rem] uppercase py-3 font-semibold">
-          Job Reference: Senior Developer
-        </p>
+        <p className="text-[0.6rem] self-end pb-5">14th August 2026</p>
         <p className="text-[0.55rem] pb-2">Dear Recipient,</p>
         <p className="text-[0.55rem] pb-2">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit dolore
@@ -42,8 +29,7 @@ function InformalLetter() {
           consectetur, tenetur consequuntur quas.
         </p>
         <p className="text-[0.55rem] pb-5">Sincerely,</p>
-        <p className="text-[0.55rem] pb-2">John Smith</p>
-        <p className="text-[0.55rem] pb-5">insert signature</p>
+        <p className="text-[0.65rem] pb-2 font-semibold">John Smith</p>
       </div>
     </div>
   );
