f62eeb8ee8a69e26206d04e1b7c6fb590ad12da5
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-31 15:33:51 +0800

    feat: add a templates route

--- app/templates/one/page.tsx
+++ app/templates/one/page.tsx
@@ -4,33 +4,33 @@ export default function page() {
   return (
     <>
       <div className="h-full w-full flex justify-center">
-        <div className="h-[85%] w-[85%] ml-2 flex flex-col gap-3 mt-[2rem]">
-          <p className="self-start bg-subyellow font-bold text-2xl  text-black tracking-wider uppercase">
-            Formal
+        <div className="h-[85%] w-fit ml-2 flex flex-col gap-5 mt-[2rem]">
+          <p className="self-start bg-subyellow font-bold text-3xl  text-black ">
+            Business/Formal Letter
           </p>
-          <div className="h-[85%] w-[95%] bg-white rounded-md shadow-gray-600 shadow-md">
+          <div className="h-[95%] w-[28rem] bg-white rounded-md shadow-gray-600 shadow-md">
             {" "}
             yes
           </div>
         </div>
       </div>
       <div className="h-full w-full flex justify-center">
-        <div className="h-[85%] w-[85%] ml-2 flex flex-col gap-3 mt-[2rem]">
-          <p className="self-start bg-subyellow font-bold text-2xl  text-black tracking-wider uppercase">
-            Formal
+        <div className="h-[85%] w-fit ml-2 flex flex-col gap-5 mt-[2rem]">
+          <p className="self-start bg-subyellow font-bold text-3xl  text-black ">
+            Business/Formal Letter #2
           </p>
-          <div className="h-[85%] w-[95%] bg-white rounded-md shadow-gray-600 shadow-md">
+          <div className="h-[95%] w-[28rem] bg-white rounded-md shadow-gray-600 shadow-md">
             {" "}
             yes
           </div>
         </div>
       </div>
       <div className="h-full w-full flex justify-center">
-        <div className="h-[85%] w-[85%] ml-2 flex flex-col gap-3 mt-[2rem]">
-          <p className="self-start bg-subyellow font-bold text-2xl  text-black tracking-wider uppercase">
-            Formal
+        <div className="h-[85%] w-fit ml-2 flex flex-col gap-5 mt-[2rem]">
+          <p className="self-start bg-subyellow font-bold text-3xl  text-black ">
+            Informal Letter
           </p>
-          <div className="h-[85%] w-[95%] bg-white rounded-md shadow-gray-600 shadow-md">
+          <div className="h-[95%] w-[28rem] bg-white rounded-md shadow-gray-600 shadow-md">
             {" "}
             yes
           </div>
