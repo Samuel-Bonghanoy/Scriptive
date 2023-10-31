e9356ef6cda24cb7c2e6188d6bd62cba6e73fb23
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-31 15:43:52 +0800

    feat: add a templates route

--- app/templates/one/page.tsx
+++ app/templates/one/page.tsx
@@ -9,8 +9,20 @@ export default function page() {
             Business/Formal Letter
           </p>
           <div className="h-[95%] w-[28rem] bg-white rounded-md shadow-gray-600 shadow-md">
-            {" "}
-            yes
+            <div className="flex flex-col px-5 py-5 text-black gap-2">
+              <div>
+                <h1 className="text-xl font-bold">John Smith</h1>
+                <div className="flex justify-between">
+                  <p className="text-xs font-semibold">Marketing Specialist</p>
+                  <div>
+                    <p className="text-[0.65rem]">+123-456-7890</p>
+                    <p className="text-[0.65rem]">johnsmith@scriptive.com</p>
+                    <p className="text-[0.65rem]">123 Anywhere St. Any City</p>
+                  </div>
+                </div>
+                <hr className="border-1 border-black mt-3" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
