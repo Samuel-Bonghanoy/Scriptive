af14658135e37ed8a594ae0980694950f4b9c753
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-31 14:35:02 +0800

    feat: add a templates route

--- app/templates/layout.tsx
+++ app/templates/layout.tsx
@@ -4,10 +4,22 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:overflow-hidden">
       <Navbar />
-      <div className="grid grid-cols-3">
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
+      <div className="bg-subyellow pl-[4rem] text-sm breadcrumbs">
+        <ul>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Documents</a>
+          </li>
+          <li>Add Document</li>
+        </ul>
+      </div>
+      <h1 className="bg-subyellow text-black text-[2.8rem] font-bold max-w-[71ch] leading-snug">
+        Choose a template
+      </h1>
+      <div className="grid grid-cols-3 h-full bg-subyellow items-center justify-items-center">
+        {children}
       </div>
     </div>
   );
---
+++ app/templates/one/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <div className="h-[80%] w-[80%] bg-white"> yes</div>;
+}
