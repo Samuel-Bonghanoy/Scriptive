dba7b34abad72bddff23672e2e636d14bacb1d66
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-12-01 12:56:23 +0800

    feat: add letter model

--- api/models/Letter.cs
+++ api/models/Letter.cs
@@ -1,20 +1,20 @@
 public class Letter 
 {
   public int Id {get; set;}
-  public string SenderName {get; set;}
-  public string SenderAddress {get; set;}
-  public string SenderTitle {get; set;}
-  public string SenderContactNum {get; set;}
-  public string SenderEmail {get; set;}
+  public string? SenderName {get; set;} 
+  public string? SenderAddress {get; set;} 
+  public string? SenderTitle {get; set;}
+  public string? SenderContactNum {get; set;}
+  public string? SenderEmail {get; set;}
 
-  public string RecipientName {get; set;}
-  public string RecipientAddress {get; set;}
-  public string RecipientTitle {get; set;}
-  public string RecipientContactNum {get; set;}
-  public string RecipientEmail {get; set;}
+  public string? RecipientName {get; set;}
+  public string? RecipientAddress {get; set;}
+  public string? RecipientTitle {get; set;}
+  public string? RecipientContactNum {get; set;}
+  public string? RecipientEmail {get; set;}
 
-   public string Content {get; set;}
-  public string Date {get; set;}
-  public string Topic {get; set;}
+  public string? Content {get; set;}
+  public string? Date {get; set;}
+  public string? Topic {get; set;}
 }
 
